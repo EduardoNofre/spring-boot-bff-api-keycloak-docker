2ca35f22b4abe56b265c51a838f0f97aa1f1dc83
eduardo.sa <nofrereis@gmail.com>
2025-04-25 16:51:47 -0300

    primeiro commit do projeto

---
+++ Spring-boot-bff/src/test/java/com/spring/boot/bff/app/SpringBootBffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.boot.bff.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
