2146ef5d1fdc22d74e073ef8e619eee55a835932
Eduardo Sa <nofrereis@gmail.com>
2025-04-26 17:17:55 -0300

    Update init_sql.sql
    atualização script

--- init_sql.sql
+++ init_sql.sql
@@ -1,55 +1,54 @@
 CREATE SCHEMA IF NOT EXISTS spring_boot_banco;
 
 CREATE TABLE IF NOT EXISTS categoria_noticias (
-    idCad INT auto_increment primary key,
-    tipo_noticias VARCHAR(50) not null,
-    tipo_noticias_desc VARCHAR(50) not null,
-    dataHrInclusao DATETIME not null,
+    id_cad INT auto_increment primary key,
+    tipo_noticias_cad VARCHAR(50) not null,
+    tipo_noticias_desc_cad VARCHAR(50) not null,
+    data_hr_inclusao DATETIME not null,
 	criado_em TIMESTAMP default CURRENT_TIMESTAMP
 
 );
 
 CREATE TABLE IF NOT EXISTS noticia_principal (
     idPrinc INT auto_increment primary key,
-    tipo_noticias VARCHAR(50) not null,
-    tipo_noticias_desc VARCHAR(50) not null,
-    idCad INT,
-    foreign key (idCad) references categoria_noticias(idCad),
-    dataHrInclusao DATETIME not null,
+    tipo_noticias_princ VARCHAR(50) not null,
+    tipo_noticias_desc_princ VARCHAR(50) not null,
+    id_cad INT,
+    foreign key (id_cad) references categoria_noticias(id_cad),
+    data_hr_inclusao DATETIME not null,
 	criado_em TIMESTAMP default CURRENT_TIMESTAMP
 
 );
 
 CREATE TABLE IF NOT EXISTS noticia_esportes (
     idEsp INT auto_increment primary key,
-    tipo_noticias VARCHAR(50) not null,
-    tipo_noticias_desc VARCHAR(50) not null,
-    idCad INT,
-    foreign key (idCad) references categoria_noticias(idCad),
-    dataHrInclusao DATETIME not null,
+    tipo_noticias_esp VARCHAR(50) not null,
+    tipo_noticias_desc_esp VARCHAR(50) not null,
+    id_cad INT,
+    foreign key (id_cad) references categoria_noticias(id_cad),
+    data_hr_inclusao DATETIME not null,
 	criado_em TIMESTAMP default CURRENT_TIMESTAMP
 
 );
 
 CREATE TABLE IF NOT EXISTS noticia_politica (
     idPol INT auto_increment primary key,
-    tipo_noticias VARCHAR(50) not null,
-    tipo_noticias_desc VARCHAR(50) not null,
-    idCad INT,
-    foreign key (idCad) references categoria_noticias(idCad),
-    dataHrInclusao DATETIME not null,
+    tipo_noticias_pol VARCHAR(50) not null,
+    tipo_noticias_desc_pol VARCHAR(50) not null,
+    id_cad INT,
+    foreign key (id_cad) references categoria_noticias(id_cad),
+    data_hr_inclusao DATETIME not null,
 	criado_em TIMESTAMP default CURRENT_TIMESTAMP
 
 );
 
 CREATE TABLE IF NOT EXISTS noticia_economia (
     idEco INT auto_increment primary key,
-    tipo_noticias VARCHAR(50) not null,
-    tipo_noticias_desc VARCHAR(50) not null,
-    idCad INT,
-    foreign key (idCad) references categoria_noticias(idCad),
-    dataHrInclusao DATETIME not null,
+    tipo_noticias_eco VARCHAR(50) not null,
+    tipo_noticias_desc_eco VARCHAR(50) not null,
+    id_cad INT,
+    foreign key (id_cad) references categoria_noticias(id_cad),
+    data_hr_inclusao DATETIME not null,
 	criado_em TIMESTAMP default CURRENT_TIMESTAMP
 );
-
 
