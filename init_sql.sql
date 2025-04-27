cdb0c8abefa6c0dadc2ede1038912a7fd6de01fb
Eduardo Sa <nofrereis@gmail.com>
2025-04-27 17:26:38 -0300

    Update init_sql.sql
    nova modelagem

--- init_sql.sql
+++ init_sql.sql
@@ -1,58 +1,37 @@
 CREATE SCHEMA IF NOT EXISTS spring_boot_banco;
 
-CREATE TABLE IF NOT EXISTS categoria_noticias (
-    id_cad INT auto_increment primary key,
+
+CREATE TABLE IF NOT EXISTS categoria(
+    id_cad INT AUTO_INCREMENT,
     tipo_noticias_cad VARCHAR(50) not null,
     tipo_noticias_desc_cad VARCHAR(50) not null,
     data_hr_inclusao DATETIME not null,
-	criado_em TIMESTAMP default CURRENT_TIMESTAMP
+	criado_em TIMESTAMP default CURRENT_TIMESTAMP,
+	PRIMARY KEY (id_cad)
 );
 
 CREATE TABLE IF NOT EXISTS noticia_principal (
-    id_princ INT auto_increment primary key,
+    id_princ INT AUTO_INCREMENT,
     tipo_noticias_princ VARCHAR(50) not null,
     tipo_noticias_desc_princ VARCHAR(50) not null,
     id_cad INT,
-    foreign key (id_cad) references categoria_noticias(id_cad),
+    foreign key (id_cad) references categoria(id_cad),
     data_hr_inclusao DATETIME not null,
 	criado_em TIMESTAMP default CURRENT_TIMESTAMP,
-	imagem_noticia_princ VARCHAR(50) not null
+	imagem_noticia_princ VARCHAR(50) not null,
+	PRIMARY KEY (id_princ)
 
 );
 
-CREATE TABLE IF NOT EXISTS noticia_esportes (
-    id_esp INT auto_increment primary key,
-    tipo_noticias_esp VARCHAR(50) not null,
-    tipo_noticias_desc_esp VARCHAR(50) not null,
+CREATE TABLE IF NOT EXISTS outras_noticias (
+    id_outras INT AUTO_INCREMENT,
+    tipo_noticias_outras VARCHAR(50) not null,
+    tipo_noticias_desc_outras VARCHAR(50) not null,
     id_cad INT,
-    foreign key (id_cad) references categoria_noticias(id_cad),
+    foreign key (id_cad) references categoria(id_cad),
     data_hr_inclusao DATETIME not null,
 	criado_em TIMESTAMP default CURRENT_TIMESTAMP,
-	imagem_noticia_esp VARCHAR(50) not null
+	imagem_noticia_outras VARCHAR(50) not null,
+	PRIMARY KEY (id_outras)
 
 );
-
-CREATE TABLE IF NOT EXISTS noticia_politica (
-    id_pol INT auto_increment primary key,
-    tipo_noticias_pol VARCHAR(50) not null,
-    tipo_noticias_desc_pol VARCHAR(50) not null,
-    id_cad INT,
-    foreign key (id_cad) references categoria_noticias(id_cad),
-    data_hr_inclusao DATETIME not null,
-	criado_em TIMESTAMP default CURRENT_TIMESTAMP,
-	imagem_noticia_pol VARCHAR(50) not null
-	
-
-);
-
-CREATE TABLE IF NOT EXISTS noticia_economia (
-    id_eco INT auto_increment primary key,
-    tipo_noticias_eco VARCHAR(50) not null,
-    tipo_noticias_desc_eco VARCHAR(50) not null,
-    id_cad INT,
-    foreign key (id_cad) references categoria_noticias(id_cad),
-    data_hr_inclusao DATETIME not null,
-	criado_em TIMESTAMP default CURRENT_TIMESTAMP,
-	imagem_noticia_eco VARCHAR(50) not null
-);
-
