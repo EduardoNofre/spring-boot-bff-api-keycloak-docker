c448a2c37229b9bc2b256dd25e3e3c2489c9c32e
Eduardo Sa <nofrereis@gmail.com>
2025-05-01 17:49:31 -0300

    Update init_sql.sql
    refator

--- init_sql.sql
+++ init_sql.sql
@@ -2,16 +2,16 @@ CREATE SCHEMA IF NOT EXISTS spring_boot_banco;
 
 
 CREATE TABLE IF NOT EXISTS categoria(
-    idCategoria INT AUTO_INCREMENT,
+    id INT AUTO_INCREMENT,
    	categoria VARCHAR(50) not null,
     categoria_desc VARCHAR(50) not null,
     data_hr_alteracao DATETIME not null,
 	criado_em TIMESTAMP default CURRENT_TIMESTAMP,
-	PRIMARY KEY (idCategoria)
+	PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS noticia(
-    idNoticia INT AUTO_INCREMENT,
+    id INT AUTO_INCREMENT,
     noticias VARCHAR(50) not null,
     noticias_desc VARCHAR(50) not null,
     categoria_id INT,
@@ -19,8 +19,8 @@ CREATE TABLE IF NOT EXISTS noticia(
     data_hr_alteracao DATETIME not null,
 	criado_em TIMESTAMP default CURRENT_TIMESTAMP,
 	imagem_noticia VARCHAR(50) not null,
-	PRIMARY KEY (idNoticia),
-	CONSTRAINT fk_categoria_01 FOREIGN KEY (categoria_id) REFERENCES categoria(idCategoria)
+	PRIMARY KEY (id),
+	CONSTRAINT fk_categoria_01 FOREIGN KEY (categoria_id) REFERENCES categoria(id)
     ON DELETE SET NULL
     ON UPDATE CASCADE
 );
