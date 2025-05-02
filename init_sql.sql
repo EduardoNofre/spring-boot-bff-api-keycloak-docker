12ab17cc3fef1754f2065d3839b211c982410a34
Eduardo Sa <nofrereis@gmail.com>
2025-05-01 23:45:56 -0300

    Update init_sql.sql
    rafctor

--- init_sql.sql
+++ init_sql.sql
@@ -2,27 +2,27 @@ CREATE SCHEMA IF NOT EXISTS spring_boot_banco;
 
 
 CREATE TABLE IF NOT EXISTS categoria(
-    id INT AUTO_INCREMENT,
-   	categoria VARCHAR(50) not null,
-    categoria_desc VARCHAR(50) not null,
-    data_hr_alteracao DATETIME not null,
+	id INT AUTO_INCREMENT,
+	categoria VARCHAR(50) not null,
+	categoria_desc VARCHAR(50) not null,
+	data_hr_alteracao DATETIME not null,
 	criado_em TIMESTAMP default CURRENT_TIMESTAMP,
-	PRIMARY KEY (id)
+PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS importancia(
-    id INT AUTO_INCREMENT,
-    importancia VARCHAR(50) not null,
-    importancias_desc VARCHAR(50) not null,
+	id INT AUTO_INCREMENT,
+	importancia VARCHAR(50) not null,
+	importancias_desc VARCHAR(50) not null,
 	criado_em TIMESTAMP default CURRENT_TIMESTAMP,
-	PRIMARY KEY (id)
+PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS noticia(
-    id INT AUTO_INCREMENT,
-    noticias VARCHAR(50) not null,
-    noticias_desc VARCHAR(50) not null,
-    data_hr_alteracao DATETIME not null,
+	id INT AUTO_INCREMENT,
+	noticias VARCHAR(50) not null,
+	noticias_desc VARCHAR(50) not null,
+	data_hr_alteracao DATETIME not null,
 	criado_em TIMESTAMP default CURRENT_TIMESTAMP,
 	imagem_noticia VARCHAR(50) not null,
 	categoria_id INT,
